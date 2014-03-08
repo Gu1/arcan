69bec92acb4779a07ad039b88aecca6f0740b787
bjornstahl <arcan-fe@nospam>
2014-03-08 21:18:17 +0100

    (avfeed) initial terminal experiment

--- frameserver/avfeed.c
+++ frameserver/avfeed.c
@@ -1,81 +1,149 @@
-/* 
- Arcan AVFeed Frameserver example 
- Copyright (c) 2014, Bjorn Stahl
- All rights reserved.
- 
- Redistribution and use in source and binary forms, 
- with or without modification, are permitted provided that the 
- following conditions are met:
- 
- 1. Redistributions of source code must retain the above copyright notice, 
- this list of conditions and the following disclaimer.
-
- 2. Redistributions in binary form must reproduce the above copyright notice, 
- this list of conditions and the following disclaimer in the documentation 
- and/or other materials provided with the distribution.
- 
- 3. Neither the name of the copyright holder nor the names of its contributors 
- may be used to endorse or promote products derived from this software without 
- specific prior written permission.
- 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
- AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
- THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
- ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
- LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, 
- OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
- SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
- INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
- CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
- ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF 
- THE POSSIBILITY OF SUCH DAMAGE.
-*/
+/*
+ * Arcan Terminal
+ * Derived from ST (suckless terminal), http://st.suckless.org 
+ */
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <string.h>
+#include <errno.h>
+
 #include <sys/types.h>
+#include <sys/ioctl.h>
+#include <signal.h>
 
-#include <arcan_shmif.h>
-#include "frameserver.h"
+#ifdef _LINUX
+	#include <pty.h>
+#elif __APPLE__
+	#include <util.h>
+#else
+	#include <libutil.h>
+#endif
 
-/*
- * Quick skeleton to map up a audio/video/input 
- * source to an arcan frameserver along with some helpers.
- */
-void arcan_frameserver_avfeed_run(const char* resource, const char* keyfile)
+#include <util.h>
+
+static struct {
+	pid_t pid;
+} shell_ctx;
+
+void setup_shell(char* type, char** opt_cmd)
 {
-/*	struct arg_arr* args = arg_unpack(resource); */
-	struct arcan_shmif_cont shms = arcan_shmif_acquire(keyfile,SHMIF_INPUT,true);
-	struct arcan_evctx inevq, outevq;
-	struct arcan_event ev;
+	char** args;
+	char *envshell = getenv("SHELL");
+	const struct passwd *pass = getpwuid(getuid());
+	char buf[sizeof(long) * 8 + 1];
 
-	arcan_shmif_setevqs(shms.addr, shms.esem, &inevq, &outevq, false);
-	int startw = 320, starth = 200;
+	unsetenv("COLUMNS");
+	unsetenv("LINES");
+	unsetenv("TERMCAP");
 
-	if (!arcan_shmif_resize(&shms, startw, starth)){
-		LOG("arcan_frameserver(decode) shmpage setup, resize failed\n");
-		return;
+	if(pass) {
+		printf("got pass, shell: %s\n", pass->pw_shell);
+		setenv("LOGNAME", pass->pw_name, 1);
+		setenv("USER", pass->pw_name, 1);
+		setenv("SHELL", pass->pw_shell, 0);
+		setenv("HOME", pass->pw_dir, 0);
 	}
-	
-	uint32_t* vidp;
-	uint16_t* audp;
+	printf("envshell? %s\n", envshell);
 
-	arcan_shmif_calcofs(shms.addr, (uint8_t**) &vidp, (uint8_t**) &audp);
-	uint32_t* cptr = vidp;
-	for (int i = 0; i < starth * startw; i++)
-		*cptr++ = 0xff000000;
-	arcan_shmif_signal(&shms, SHMIF_SIGVID);
+/* WINDOWID?
+	setenv("WINDOWID", buf, 1);
+*/
 
-	while(1){
-		while (1 == arcan_event_poll(&inevq, &ev)){
+	signal(SIGCHLD, SIG_DFL);
+	signal(SIGHUP, SIG_DFL);
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTERM, SIG_DFL);
+	signal(SIGALRM, SIG_DFL);
+
+	setenv("TERM", strdup(type), 1);
+	args = opt_cmd ? opt_cmd : (char *[]){envshell, "-i", NULL}; 
+	execvp(args[0], args); 
+	exit(EXIT_FAILURE);
+}
+
+void sigchld(int a)
+{
+	int stat = 0;
+	printf("sigchild\n");
+	if (waitpid(shell_ctx.pid, &stat, 0) < 0)
+		exit(EXIT_FAILURE);
+
+	if (WIFEXITED(stat))
+		exit(WEXITSTATUS(stat));
+	else
+		exit(EXIT_FAILURE);
+}
+
+#ifdef TERM_STANDALONE
+int main(int argc, char** argv)
+#else
+void arcan_frameserver_avfeed_run(const char* resource, const char* keyfike) 
+#endif
+{
+/* allocate tty */
+	struct winsize w = {80, 25, 0, 0};
+	struct timeval* tv;
+	int m, s;
+
+	if (openpty(&m, &s, NULL, NULL, &w) < 0)
+		goto error;
+
 /*
- *	event dispatch loop, look at shmpage interop,
- *	valid categories here should (at least)
- *	be EVENT_SYSTEM, EVENT_IO, EVENT_TARGET
- */ 
+ * clone shell, forward STDIN/STDOUT/STDERR
+ */
+	printf("forking\n");
+	switch(shell_ctx.pid = fork()){
+	case -1:
+		goto error;
+	break;
+	case 0:
+		printf("setup shell\n");
+		dup2(s, STDIN_FILENO);
+		dup2(s, STDOUT_FILENO);
+		dup2(s, STDERR_FILENO);
+		if (ioctl(s, TIOCSCTTY, NULL) < 0)
+			exit(EXIT_FAILURE);
+		close(s);
+		close(m);
+		setup_shell("/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal"
+									, NULL);
+	break;
+	default:
+		close(s);
+		signal(SIGCHLD, sigchld);
+	}
+
+/*
+ * use the parent socket as a trigger for sweeping
+ * the event queue, 
+ */	
+	fd_set rfd;
+	while(1){
+		FD_ZERO(&rfd);
+		FD_SET(m, &rfd);
+
+		printf("selecting\n");
+		if (select(1, &rfd, NULL, NULL, tv) < 0){
+			if (errno == EINTR)
+				continue;
+			printf("select failed\n");
+			break;
+		}
+		
+		if (FD_ISSET(m, &rfd)){
+/* ttyread */
 		}
 	}
+
+error:
+	m = 1;
+#ifdef TERM_STANDALONE
+	return 0;
+#else
+#endif
 }
