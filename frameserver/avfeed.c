e3b2e6b2e21c7fc175084249779fbe04d3c0ff7f
bjornstahl <arcan-fe@nospam>
2014-03-09 19:42:48 +0100

    additional terminal work, still ways to go

--- frameserver/avfeed.c
+++ frameserver/avfeed.c
@@ -1,6 +1,18 @@
 /*
- * Arcan Terminal
- * Derived from ST (suckless terminal), http://st.suckless.org 
+ * Arcan Terminal (AT)
+ * ----------------------
+ *
+ * Derived from ST (suckless terminal), http://st.suckless.org
+ * The UTF8 routines, terminal management and sequence parsing
+ * could mostly be lifted, while-as everything graphics and input
+ * related had to be rewritten.
+ *
+ * The basic design works around re-using the format string
+ * renderfunctions from the main engine. With that, we get TTF
+ * caching and most visual features needed. Thus, the history
+ * buffer contains lines in their original form, then a parse
+ * routine emits format strings, which gets rendered into the
+ * shmpage directly. 
  */
 
 #include <stdlib.h>
@@ -11,43 +23,49 @@
 #include <string.h>
 #include <errno.h>
 
+#include <arcan_shmif.h>
+#include "frameserver.h"
+
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/ioctl.h>
 #include <signal.h>
+#include <pwd.h>
 
 #ifdef _LINUX
 	#include <pty.h>
 #elif __APPLE__
 	#include <util.h>
-#else
+#elif __FreeBSD
 	#include <libutil.h>
 #endif
 
-#include <util.h>
-
 static struct {
-	pid_t pid;
-} shell_ctx;
+	struct arcan_shmif_cont shmcont;
+	struct arcan_evctx inevq;
+	struct arcan_evctx outevq;
 
-void setup_shell(char* type, char** opt_cmd)
+	pid_t pid;
+	
+} term_ctx;
+
+void setup_shell(const char* type, char** opt_cmd)
 {
 	char** args;
 	char *envshell = getenv("SHELL");
 	const struct passwd *pass = getpwuid(getuid());
-	char buf[sizeof(long) * 8 + 1];
+//	char buf[sizeof(long) * 8 + 1];
 
 	unsetenv("COLUMNS");
 	unsetenv("LINES");
 	unsetenv("TERMCAP");
 
 	if(pass) {
-		printf("got pass, shell: %s\n", pass->pw_shell);
 		setenv("LOGNAME", pass->pw_name, 1);
 		setenv("USER", pass->pw_name, 1);
 		setenv("SHELL", pass->pw_shell, 0);
 		setenv("HOME", pass->pw_dir, 0);
 	}
-	printf("envshell? %s\n", envshell);
 
 /* WINDOWID?
 	setenv("WINDOWID", buf, 1);
@@ -69,8 +87,8 @@ void setup_shell(char* type, char** opt_cmd)
 void sigchld(int a)
 {
 	int stat = 0;
-	printf("sigchild\n");
-	if (waitpid(shell_ctx.pid, &stat, 0) < 0)
+	LOG("sigchild\n");
+	if (waitpid(term_ctx.pid, &stat, 0) < 0)
 		exit(EXIT_FAILURE);
 
 	if (WIFEXITED(stat))
@@ -79,43 +97,69 @@ void sigchld(int a)
 		exit(EXIT_FAILURE);
 }
 
-#ifdef TERM_STANDALONE
-int main(int argc, char** argv)
-#else
-void arcan_frameserver_avfeed_run(const char* resource, const char* keyfike) 
-#endif
+void setup_shmterm(struct arg_arr* inargs, const char* keyfile)
+{
+	int desw = 320;
+	int desh = 200;
+
+	term_ctx.shmcont = arcan_shmif_acquire(keyfile,SHMIF_INPUT, true);
+
+	arcan_shmif_setevqs(term_ctx.shmcont.addr, term_ctx.shmcont.esem, 
+		&(term_ctx.inevq), &(term_ctx.outevq), false);
+
+	if (!arcan_shmif_resize(&term_ctx.shmcont, desw, desh)){
+		LOG("failed to setup shmpage rendering, requested: %d, %d\n",
+			desw, desh);
+	}
+/*
+ * default resolution, font etc. as possible args 
+ */ 
+}
+
+void arcan_frameserver_avfeed_run(const char* resource, const char* keyfile)
 {
 /* allocate tty */
-	struct winsize w = {80, 25, 0, 0};
-	struct timeval* tv;
-	int m, s;
+	struct arg_arr* args = arg_unpack(resource);
+	const char* shell;
+	if (!arg_lookup(args, "shell", 0, &shell))
+		shell = "/bin/sh";
 
-	if (openpty(&m, &s, NULL, NULL, &w) < 0)
-		goto error;
+	struct winsize wnd_sz = {80, 25, 0, 0};
+	struct timeval* tv = NULL;
+	int master, slave;
+
+	if (openpty(&master, &slave, NULL, NULL, &wnd_sz) < 0){
+		LOG("couldn't open shell\n");
+		return;
+	}
 
 /*
  * clone shell, forward STDIN/STDOUT/STDERR
  */
-	printf("forking\n");
-	switch(shell_ctx.pid = fork()){
+	switch(term_ctx.pid = fork()){
 	case -1:
-		goto error;
-	break;
+		LOG("spawning new shell failed (fork)\n");
+		return;
+	
 	case 0:
-		printf("setup shell\n");
-		dup2(s, STDIN_FILENO);
-		dup2(s, STDOUT_FILENO);
-		dup2(s, STDERR_FILENO);
-		if (ioctl(s, TIOCSCTTY, NULL) < 0)
+		setsid();
+		dup2(slave, STDIN_FILENO);
+		dup2(slave, STDOUT_FILENO);
+		dup2(slave, STDERR_FILENO);
+
+		if (ioctl(slave, TIOCSCTTY, NULL) < 0){
+			LOG("(child) failed setting TIOCSCTTY: %s\n", strerror(errno));
 			exit(EXIT_FAILURE);
-		close(s);
-		close(m);
-		setup_shell("/Applications/Utilities/Terminal.app/Contents/MacOS/Terminal"
-									, NULL);
+		}
+
+		close(slave);
+		close(master);
+		setup_shell(shell, NULL);
 	break;
 	default:
-		close(s);
+		close(slave);
 		signal(SIGCHLD, sigchld);
+		setup_shmterm(args, keyfile);
 	}
 
 /*
@@ -125,25 +169,18 @@ void arcan_frameserver_avfeed_run(const char* resource, const char* keyfike)
 	fd_set rfd;
 	while(1){
 		FD_ZERO(&rfd);
-		FD_SET(m, &rfd);
+		FD_SET(master, &rfd);
 
-		printf("selecting\n");
+		LOG("selecting\n");
 		if (select(1, &rfd, NULL, NULL, tv) < 0){
 			if (errno == EINTR)
 				continue;
-			printf("select failed\n");
+			LOG("select failed\n");
 			break;
 		}
 		
-		if (FD_ISSET(m, &rfd)){
-/* ttyread */
+		if (FD_ISSET(master, &rfd)){
+			
 		}
-	}
-
-error:
-	m = 1;
-#ifdef TERM_STANDALONE
-	return 0;
-#else
-#endif
+	}	
 }
